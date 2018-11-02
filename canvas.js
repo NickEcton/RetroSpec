a0d5d82b024639d1e81bd6eb280f1eeb9796d8fe
Nick <Ecton.Nick@gmail.com>
2018-11-02 11:15:42 -0700

    add color gradients

--- canvas.js
+++ canvas.js
@@ -1,4 +1,64 @@
 //Instantiate bufferLoader class
+var colour = [
+    [62,35,255],
+    [60,255,60],
+    [255,35,98],
+    [45,175,230],
+    [255,0,255],
+    [255,128,0]
+];
+  var step = 0;
+  //color table indices for: 
+  // current color left
+  // next color left
+  // current color right
+  // next color right
+  var colorIndices = [0,1,2,3];
+  
+  //transition speed
+  var gradientSpeed = 0.002;
+  
+  function updateGradient()
+  {
+    
+
+    
+  var c0_0 = colour[colorIndices[0]];
+  var c0_1 = colour[colorIndices[1]];
+  var c1_0 = colour[colorIndices[2]];
+  var c1_1 = colour[colorIndices[3]];
+  
+  var istep = 1 - step;
+  var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+  var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+  var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+  var color1 = "rgb("+r1+","+g1+","+b1+")";
+
+  var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+  var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+  var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+  var color2 = "rgb("+r2+","+g2+","+b2+")";
+  
+   document.querySelector('canvas').style.background = "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"
+
+    step += gradientSpeed;
+    if ( step >= 1 )
+    {
+      step %= 1;
+      colorIndices[0] = colorIndices[1];
+      colorIndices[2] = colorIndices[3];
+      
+      //pick two new target color indices
+      //do not pick the same as the current one
+      colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colour.length - 1))) % colour.length;
+      colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colour.length - 1))) % colour.length;
+      
+    }
+  }
+  
+  setInterval(updateGradient,10);
+
+
 
 function BufferLoader(context, urlList, callback) {
     this.context = context;
@@ -151,7 +211,7 @@ c.clearRect(0, 0, innerWidth, innerHeight)
     }
 
     // Creates middle Circle // 
-    console.log(dataArray[100])
+
     circleRadius = dataArray[3] / 2
     c.strokeStyle = "rgb(255,105,180)";
     c.beginPath()
@@ -225,7 +285,7 @@ dropzone.addEventListener("drop", function( event ) {
 
         dt = event.dataTransfer
         files = dt.files
-        console.log(event.dataTransfer.files[0])
+
         
         document.querySelector('audio').src = event.dataTransfer.files[0].name
         document.querySelector('audio').play();
